c8d61bdee38f89d1201f6f139e79814d33b86606
박형주 <hyeongju1122@gmail.com>
2023-11-22 02:35:12 +0900

    :rocket: docker & Action Runner 추가

--- src/main/java/com/uok/avod/controller/ApiController.java
+++ src/main/java/com/uok/avod/controller/ApiController.java
@@ -3,6 +3,7 @@ package com.uok.avod.controller;
 import com.uok.avod.service.ApiService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,11 @@ public class ApiController {
 
     ApiController(ApiService apiService) {
         this.apiService = apiService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("eroom 배포 자동화 테스트");
     }
 
     @PostMapping("/token")
